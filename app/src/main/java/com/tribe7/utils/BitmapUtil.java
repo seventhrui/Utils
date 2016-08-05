904f8c053450832e4bfa4e938175985257e073e7
seventh <gao_rui2011@163.com>
2016-08-05 16:08:56 +0800

    初始化

---
+++ app/src/main/java/com/tribe7/utils/BitmapUtil.java
@@ -0,0 +1,35 @@
+package com.tribe7.utils;
+
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.PixelFormat;
+import android.graphics.drawable.Drawable;
+
+/**
+ * bitmap处理工具类
+ *
+ */
+public class BitmapUtil {
+
+    public static Bitmap drawableToBitmap(Drawable drawable) {
+        Bitmap bitmap = Bitmap.createBitmap(
+
+                drawable.getIntrinsicWidth(),
+
+                drawable.getIntrinsicHeight(),
+
+                drawable.getOpacity() != PixelFormat.OPAQUE ? Bitmap.Config.ARGB_8888
+
+                        : Bitmap.Config.RGB_565);
+
+        Canvas canvas = new Canvas(bitmap);
+
+        //canvas.setBitmap(bitmap);
+
+        drawable.setBounds(0, 0, drawable.getIntrinsicWidth(), drawable.getIntrinsicHeight());
+
+        drawable.draw(canvas);
+
+        return bitmap;
+    }
+}
